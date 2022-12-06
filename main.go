84ff4ec0d9e619e7ba1a340013839c577c22acbb
Nathanael Abel <yujioshinozaki@gmail.com>
2022-12-06 13:25:08 +0700

    db.Init() on main

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
-import "depd_go_echo/routes"
+import (
+	"depd_go_echo/db"
+	"depd_go_echo/routes"
+)
 
 func main() {
+	db.Init()
 	e := routes.Init()
 	e.Logger.Fatal(e.Start(":8000"))
 }
